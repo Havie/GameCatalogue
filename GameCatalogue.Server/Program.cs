218d2333c32d12564fb476853ab61a0be1ab5abf
Steve Datz <sfdatz@live.com>
2023-09-30 16:13:44 -0400

    Server side games, REST client
    Created copy of the Games Class on Server (seems terrible practice to have 2 files that need to stay synced, instead of references in one single area.. but figure out how to not violate DRY another time)
    
    Install and use Huachao Mao's REST client Extension
    
    Inside of the games.http file you can click an intellisense header for "Send Request" above the GET line for VS Code to simulate.

--- GameCatalogue.Server/Program.cs
+++ GameCatalogue.Server/Program.cs
@@ -1,6 +1,20 @@
+using GameCatalogue.Server.Models;
+
+//Server side games 
+List<Game> _games = new() 
+  {
+        //example data
+        new Game() { Id = 1, GameName = "Super Mario", Genre = "Platformer", Price = 59.99m, ReleaseDate = new DateTime(1985,9,13)},
+        new Game() { Id = 2, GameName = "Street Fighter", Genre = "Fighting", Price = 59.99m, ReleaseDate = new DateTime(1991,2,1)},
+        new Game() { Id = 3, GameName = "Notoris: The Goblin War", Genre = "Strategy", Price = 4.99m, ReleaseDate = new DateTime(2021,7, 1)},
+  };
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//GET /games
+app.MapGet("/games", () => _games);
 
 app.Run();
