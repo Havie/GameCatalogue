3f5f1e4c1ba1ee9755642b177ff7041e61358d76
Steve Datz <sfdatz@live.com>
2023-10-01 10:25:53 -0400

    Update Client to send Async server requests, have CORS error on connection (todo-next)

--- GameCatalogue.Client/GameClient.cs
+++ GameCatalogue.Client/GameClient.cs
@@ -1,47 +1,44 @@
 using GameCatalogue.Client.Models;
+using System.Net.Http.Json; //methods to retrieve/send json data thru API
+
 
 namespace GameCatalogue.Client;
 
-public static class GameClient
+public  class GameClient
 {
-
-  public static Game[] GetGames() => _games.ToArray();  
-  public static IReadOnlyList<Game> Games => _games;
-  private static readonly List<Game> _games = new() 
+  private readonly HttpClient _httpClient;
+   /*******************************************************************************/
+  public GameClient(HttpClient httpClient)
   {
-        //Static example data
-    
-        new Game() { Id = 1, GameName = "Super Mario", Genre = "Platformer", Price = 59.99m, ReleaseDate = new DateTime(1985,9,13)},
-        new Game() { Id = 2, GameName = "Street Fighter", Genre = "Fighting", Price = 59.99m, ReleaseDate = new DateTime(1991,2,1)},
-        new Game() { Id = 3, GameName = "Notoris: The Goblin War", Genre = "Strategy", Price = 4.99m, ReleaseDate = new DateTime(2021,7, 1)},
-  };
-
-  public static Game GetGame(int gameID)
+    _httpClient = httpClient;
+  }
+  /*******************************************************************************/
+  public async Task<Game[]?> GetGamesAsync() 
   {
-    return _games.Find(game => game.Id == gameID) ?? throw new Exception($"Game with id #{gameID} not found");
+    //string urlRoute = "/games" without the / in GameServer.Program 
+    return await _httpClient.GetFromJsonAsync<Game[]>("games");
+  } 
+
+  public async Task<Game?> GetGameAsync(int gameID)
+  {
+    return await _httpClient.GetFromJsonAsync<Game>($"games/{gameID}") ?? throw new Exception("Failed to find game");
   }
 
-  public static void AddGame(Game newGame) 
+  public async Task AddGameAsync(Game newGame) 
   {
-      //newGame.Id = _games.Count;
-      newGame.Id = Games.Max(x => x.Id) + 1; //instead of using count, find the largest ID and add one to it.
-     _games.Add(newGame);
+     await _httpClient.PostAsJsonAsync<Game>("games", newGame);
   }
 
-  public static void UpdateGame(Game updatedGame)
+  public async Task UpdateGameAsync(Game updatedGame)
   {
-    var existingGame = GetGame(updatedGame.Id); //throws an exception if not found
-    //Update the existing games fields with the new values
-    existingGame.GameName = updatedGame.GameName;
-    existingGame.Genre = updatedGame.Genre;
-    existingGame.Price = updatedGame.Price;
-    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    await _httpClient.PutAsJsonAsync<Game>($"games/{updatedGame.Id}", updatedGame);
   }
 
-  public static void DeleteGame(int gameID)
+  public async Task DeleteGameAsync(int gameID)
  {
-    var existingGame = GetGame(gameID); //throws an exception if not found
-    _games.Remove(existingGame);  
+    await _httpClient.DeleteFromJsonAsync<Game>($"games/{gameID}");
  }
+
+  /*******************************************************************************/
 
 }
--- GameCatalogue.Client/Program.cs
+++ GameCatalogue.Client/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//was the client
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//change to the server (GameCatalogue.Server->Properties->launchSettings.json->profiles applicationUrl)
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5030") });
+//inject a "static" instance of the GameClient class. Basically a singleton
+builder.Services.AddScoped<GameClient>();
 
 await builder.Build().RunAsync();
